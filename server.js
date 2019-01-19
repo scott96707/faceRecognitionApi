195f1ec07bf69e95c451e06e1f76ee902587715d
scott96707 <scott96707@gmail.com>
2019-01-19 14:55:54 -0700

    Fixed url for database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.postgresql-colorful-60856,
         SSL: true,
     }
 });
