e5ac86b17762ebbb9ebd63dd2d356a789df24e73
scott96707 <scott96707@gmail.com>
2019-01-19 14:21:28 -0700

    Updating database to Heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-colorful-60856',
     user : 'postgres',
     password : '',
     database : 'smart-brain'
