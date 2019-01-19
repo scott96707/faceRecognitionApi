bcded4777684b36cded5612fadf4d6922e5fdc8a
scott96707 <scott96707@gmail.com>
2019-01-19 13:55:04 -0700

    Fixing Heroku deployment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send(`It's working!`) });
 app.post('/signin', signin.handleSignin(bcrypt, db));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfile (req, res, db) });
